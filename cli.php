8d97704b3cb9cf4f3ae506db474ed87b4b975137
Barinulka <barinulka@yandex.ru>
2023-05-03 11:37:40 +0400

    add comment

--- cli.php
+++ cli.php
@@ -21,6 +21,8 @@ $usersRepository = new MysqlUsersRepository($connection);
 // $usersRepository->save(new User(UUID::random(), new Name('Super', 'Admin'), 'admin'));
 $command = new CreateUserCommand($usersRepository);
 
+// command: php cli.php login=some_login first_name=some_name last_name=some_last_name
+
 try {
     // echo $usersRepository->getByLogin('admin');
     $command->handle(Arguments::fromArgv($argv));
