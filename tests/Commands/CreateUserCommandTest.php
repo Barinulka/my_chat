dc41df5ea0b2c0acd095d52fe01c06dbde24d85f
Barinulka <barinulka@yandex.ru>
2023-05-03 15:45:18 +0400

    make tests with anonymous class

--- tests/Commands/CreateUserCommandTest.php
+++ tests/Commands/CreateUserCommandTest.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Blog\PHPUnit\Commands;
 
+use App\Blog\Exceptions\ArgumentsException;
+use App\Blog\Exceptions\UserNotFoundException;
+use App\Blog\User;
+use App\Blog\UUID;
 use PHPUnit\Framework\TestCase;
 use App\Blog\Commands\Arguments;
 use App\Blog\Commands\CreateUserCommand;
 use App\Blog\Exceptions\CommandException;
 use App\Blog\Repositories\UsersRepository\DummyUsersRepository;
+use App\Blog\Repositories\UsersRepository\UsersRepositoryInterface;
 
 class CreateUserCommandTest extends TestCase
 {
@@ -23,5 +28,56 @@ class CreateUserCommandTest extends TestCase
         $this->expectExceptionMessage('Пользователь с таким логином уже существует: Ivan');
 
         $command->handle(new Arguments(['login' => 'Ivan']));
+    }
+
+
+    /**
+     * Проверяем, что команда действительно требует имя пользователя
+     */
+    public function testItRequiresFirstName(): void 
+    {
+        $command = new CreateUserCommand($this->makeUsersRepository());
+
+        $this->expectException(ArgumentsException::class);
+        $this->expectExceptionMessage("Нет такого аргумента: first_name");
+        
+        $command->handle(new Arguments(['login' => 'Ivan']));
+
+    }
+
+    /**
+     * Проверяем, что команда действительно требует фамилию пользователя
+     */
+    public function testItRequiresLastName(): void 
+    {
+        $command = new CreateUserCommand($this->makeUsersRepository());
+
+        $this->expectException(ArgumentsException::class);
+        $this->expectExceptionMessage("Нет такого аргумента: last_name");
+        
+        $command->handle(new Arguments(['login' => 'Ivan', 'first_name' => 'Ivan']));
+
+    }
+
+    /**
+     * Создание анонимного класса зависящего от UsersRepositoryInterface
+     * @return UsersRepositoryInterface
+     */
+    private function makeUsersRepository(): UsersRepositoryInterface
+    {
+       return new class implements UsersRepositoryInterface {
+            public function save(User $user): void {
+                // TODO
+            }
+            public function get(UUID $uuid): User 
+            {
+                throw new UserNotFoundException("Not Found");
+            }
+
+            public function getByLogin(string $login): User 
+            {
+                throw new UserNotFoundException("Not Found");
+            }
+        };
     }
 }
